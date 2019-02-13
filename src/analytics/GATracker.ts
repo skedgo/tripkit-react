899303eeffbf7d604b667005c7f2ff1e37e0d1a1
MauroGomez <mauro@buzzhives.com>
2019-02-13 10:47:53 -0300

    10757: only production should report to analytics

--- src/analytics/GATracker.ts
+++ src/analytics/GATracker.ts
@@ -12,15 +12,18 @@ class GATracker {
     }
 
     public send(category: string, action: string, label: string) {
-        // i tried:
-        // import { ga } from "google.analytics" - Module not found
-        // create Analytics.js with the ga file: Class name must be in pascal case (and some other error i lost)
-        // window.ga - Property 'ga' does not exist on type 'Window'.
-        // disabling just no-unused-expressions
-        // this is the only thing that works!
-        // don't want to extract to a new file as i'm afraid to spend another hour on this
-        // tslint:disable-next-line
-        window['ga'] && window['ga']('send', 'event', category, action, label);
+        // 10757 request, only produdction should report to analytics
+        if (Environment.isProd() && !Environment.isStaging()) {
+            // i tried:
+            // import { ga } from "google.analytics" - Module not found
+            // create Analytics.js with the ga file: Class name must be in pascal case (and some other error i lost)
+            // window.ga - Property 'ga' does not exist on type 'Window'.
+            // disabling just no-unused-expressions
+            // this is the only thing that works!
+            // don't want to extract to a new file as i'm afraid to spend another hour on this
+            // tslint:disable-next-line
+            window['ga'] && window['ga']('send', 'event', category, action, label);
+        }
         if (Environment.isDev()) {
             console.log("GA Track event -" + " category: " + category + ", action: " + action + ", label: " + label + ".");
         }
