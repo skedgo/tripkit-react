4a5e2de94ac33e49875b845d9783a50054b065b3
MauroGomez <mauro@buzzhives.com>
2019-02-18 19:39:23 -0300

    Build 2 after: - Fix to interface name not starting with "I". - Last segment shapes are drawn twice, so it appears to have less transparency.

---
+++ dist/trip/ITripRowProps.d.ts
@@ -0,0 +1,13 @@
+import Trip from "../model/trip/Trip";
+import { EventEmitter } from "fbemitter";
+interface ITripRowProps {
+    value: Trip;
+    className?: string;
+    brief?: boolean;
+    onClick?: () => void;
+    onFocus?: () => void;
+    onKeyDown?: (e: any) => void;
+    eventBus?: EventEmitter;
+}
+export declare const TRIP_ALT_PICKED_EVENT = "onTripAltPicked";
+export default ITripRowProps;
