0c658cb06385eeec3cc3f7c3491269948fc5901c
MauroGomez <mauro@buzzhives.com>
2019-03-19 09:28:36 -0300

    Feature #10961: Stage 2: Include stop information; Departure board: - when first departures arrive automatically scroll to next departure - day separators

--- src/service/ServiceDepartureTable.tsx
+++ src/service/ServiceDepartureTable.tsx
@@ -5,16 +5,27 @@ import IServiceDepartureRowProps from "./IServiceDepartureRowProps";
 import "./ServiceDepartureTable.css";
 import IconGlass from "-!svg-react-loader!../images/ic-glass.svg";
 import {ChangeEvent} from "react";
+import DateTimeUtil from "../util/DateTimeUtil";
+import DaySeparator from "./DaySeparator";
 
 interface IProps extends IServiceResConsumerProps {
     renderDeparture: <P extends IServiceDepartureRowProps>(departureProps: P) => JSX.Element;
     className?: string;
 }
 
-class ServiceDepartureTable extends React.Component<IProps, {}> {
+interface IState {
+    scrollTop: number;
+}
+
+class ServiceDepartureTable extends React.Component<IProps, IState> {
+
+    private scrollRef: any;
 
     constructor(props: IProps) {
         super(props);
+        this.state = {
+            scrollTop: 0
+        };
         if (props.onRequestMore) {
             props.onRequestMore();
         }
@@ -28,6 +39,7 @@ class ServiceDepartureTable extends React.Component<IProps, {}> {
             console.log("requestMore!");
             this.props.onRequestMore();
         }
+        this.setState({ scrollTop: scrollPanel.scrollTop });
     }
 
     private onFilterChange(e: ChangeEvent<HTMLInputElement>) {
@@ -46,16 +58,40 @@ class ServiceDepartureTable extends React.Component<IProps, {}> {
                 </div>
                 <div className={"ServiceDepartureTable-container gl-flex gl-column gl-grow" + (this.props.className ? " " + this.props.className : "")}
                      onScroll={this.onScroll}
+                     ref={(scrollRef: any) => this.scrollRef = scrollRef}
                 >
-                    {this.props.departures.map((departure: ServiceDeparture, index: number) =>
-                        this.props.renderDeparture({
+                    {this.props.departures.reduce((elems: JSX.Element[], departure: ServiceDeparture, i: number) => {
+                        const showDayLabel = i === 0 ||
+                            DateTimeUtil.momentTZTime(this.props.departures[i - 1].actualStartTime * 1000).format("ddd D") !==
+                            DateTimeUtil.momentTZTime(departure.actualStartTime * 1000).format("ddd D");
+                        if (showDayLabel) {
+                            elems.push(<DaySeparator date={DateTimeUtil.momentTZTime(departure.actualStartTime * 1000)}
+                                                     key={"day-" + i}
+                                                     scrollTop={this.state.scrollTop + 48} // 48px of filter panel. See how to avoid this.
+                            />)
+                        }
+                        elems.push(this.props.renderDeparture({
                             value: departure,
-                            key: index
-                        })
-                    )}
+                            key: i
+                        }));
+                        return elems;
+                    }, [])}
                 </div>
             </div>
         );
+    }
+
+    public componentDidUpdate(prevProps: Readonly<IProps>, prevState: Readonly<{}>, snapshot?: any): void {
+        if (prevProps.departures.length === 0 && this.props.departures.length > 0) {
+            const now = DateTimeUtil.getNow().valueOf()/1000;
+            const nextDepartureIndex = this.props.departures.findIndex((departure: ServiceDeparture) => {
+                console.log("nextDepartureIndex: " + nextDepartureIndex);
+                return departure.actualStartTime >= now;
+            });
+            if (nextDepartureIndex && this.scrollRef) {
+                this.scrollRef.children[nextDepartureIndex].scrollIntoView();
+            }
+        }
     }
 
 }
