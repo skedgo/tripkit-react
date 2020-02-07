0f052ff674353314d610a122dff66f4ce429f8f0
MauroGomez <mauro@buzzhives.com>
2020-02-07 18:48:08 -0300

    Tripkit web sdk: - delete dist

--- dist/env/Features.d.ts
+++
@@ -1,7 +0,0 @@
-declare class Features {
-    protected static _instance: Features;
-    static initialize(): void;
-    static readonly instance: Features;
-    lightRail(): boolean;
-}
-export default Features;
--- dist/model/location/City.d.ts
+++
@@ -1,8 +0,0 @@
-import Location from "../Location";
-declare class City extends Location {
-    private _title;
-    private _timezone;
-    readonly name: string;
-    readonly timezone: string;
-}
-export default City;
