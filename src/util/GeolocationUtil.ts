7f1c741ebf9795f77118460f614d5a869fd3ce93
MauroGomez <mauro@buzzhives.com>
2020-03-12 19:07:24 -0300

    Tripkit web sdk: Feature #12584 - "My Location" issue on PC. Consider accuracy threshold. (2)

--- src/util/GeolocationUtil.ts
+++ src/util/GeolocationUtil.ts
@@ -12,10 +12,14 @@ function tKRequestIPLocation(): Promise<[number, number]> {
 
 
 /**
- * @returns {Promise<boolean | undefined>} where true means 'granted', false means 'denied' and undefined means 'prompt'
+ * @returns {Promise<boolean | undefined>} where
+ * null means permissions api is unsupported,
+ * true means 'granted',
+ * false means 'denied', and
+ * undefined means 'prompt'
  */
 
-export function tKCheckGeolocationPermission(): Promise<boolean | undefined> {
+export function tKCheckGeolocationPermission(): Promise<boolean | undefined | null> {
     return new Promise(function (resolve, reject) {
         navigator.permissions ?
             // Permission API is supported
@@ -26,14 +30,15 @@ export function tKCheckGeolocationPermission(): Promise<boolean | undefined> {
                 resolve(permission.state === "granted" ? true : permission.state === "denied" ? false : undefined)
             }) :
             // Permission API is not supported
-            reject(new Error("Permission API is not supported"))
+            resolve(null);
     })
 }
 
 // TODO: maybe use a strings enum.
 // Adv: better type checking.
 // Disadv: need to centralize error code listing in the enum.
-export const ERROR_GEOLOC_INACCURATE = "GEOLOC_INNACURATE";
+export const ERROR_GEOLOC_INACCURATE = "ERROR_GEOLOC_INNACURATE";
+export const ERROR_GEOLOC_DENIED = "ERROR_GEOLOC_DENIED";
 
 function requestCurrentLocationHTML5(accuracyThreshold?: number): Promise<[number, number]> {
     return new Promise(function (resolve, reject) {
@@ -41,7 +46,6 @@ function requestCurrentLocationHTML5(accuracyThreshold?: number): Promise<[numbe
             navigator.geolocation.getCurrentPosition(
                 // Success callback
                 function (result) {
-                    console.log(result);
                     if (accuracyThreshold === undefined || result.coords.accuracy < accuracyThreshold) {
                         resolve([result.coords.latitude, result.coords.longitude]);
                     } else {
@@ -49,30 +53,24 @@ function requestCurrentLocationHTML5(accuracyThreshold?: number): Promise<[numbe
                     }
                 },
                 // Error callback
-                function (error) { reject(error) });
+                function (error) {
+                    reject(new TKError(error.message, ERROR_GEOLOC_DENIED));
+                });
         }});
 }
 
 function tKRequestCurrentLocationHTML5(dontAskUser: boolean = false, accuracyThreshold?: number): Promise<[number, number]> {
-    if (dontAskUser) {
-        return tKCheckGeolocationPermission()
-            .catch(function () {
-                // Permission API is not supported
-                throw new Error('Unable to get HTML5 geolocation without (possibly) asking the user.');
-            })
-            .then(function (granted: boolean | undefined) {
-                // Permission API is supported
-                if (granted) {
-                    return requestCurrentLocationHTML5(accuracyThreshold);
-                } else {
-                    throw granted === false ? new Error('Location tracking blocked by user.') :
-                        new Error('Unable to get HTML5 geolocation without asking the user.');
-                }
-            })
-        // return this.requestCurrentLocationGMaps();
-    }
-    // It's ok to ask the user
-    return requestCurrentLocationHTML5(accuracyThreshold);
+    return tKCheckGeolocationPermission()
+        .then(function (granted: boolean | undefined | null) {
+            if (granted ||  // Permission granted
+                ((granted === null || granted === undefined) && !dontAskUser)) { // Permission API unsupported or didn't prompt user, and can ask user.
+                return requestCurrentLocationHTML5(accuracyThreshold);
+            } else { // granted === false || ((granted === null || granted === undefined) && dontAskUser)
+                throw granted === false ? new TKError("Location tracking blocked by user.", ERROR_GEOLOC_DENIED) :
+                    granted === null ? new Error("Permission API is not supported, unable to get HTML5 geolocation without (possibly) asking the user.") :
+                        new Error("Unable to get HTML5 geolocation without asking the user.");
+            }
+        });
 }
 
 export function tKRequestCurrentLocation(dontAskUser: boolean = false, accuracyThreshold?: number, fallback: boolean = false): Promise<[number, number]> {
