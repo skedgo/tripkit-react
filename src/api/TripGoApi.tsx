d69f300d0340055051334d11619d8fbd0b479e28
MauroGomez <mauro@buzzhives.com>
2020-04-21 09:38:18 -0300

    - fix to merge conflicts.

--- src/api/TripGoApi.tsx
+++ src/api/TripGoApi.tsx
@@ -4,7 +4,8 @@ import NetworkUtil from "../util/NetworkUtil";
 import {JsonConvert} from "json2typescript";
 import Trip from "../model/trip/Trip";
 import StopLocation from "../model/StopLocation";
-import Environment from "../env/Environment";
+import Environment, {Env} from "../env/Environment";
+import Util from "../util/Util";
 
 class TripGoApi {
 
@@ -25,18 +26,25 @@ class TripGoApi {
         return this.SATAPP;
     }
 
-    public static apiCall(endpoint: string, method: string, body?: any): Promise<any> {
+    public static apiCall(endpoint: string, method: string, body?: any, cache: boolean = false): Promise<any> {
         const url = this.getSatappUrl(endpoint);
-        return this.apiCallUrl(url, method, body);
+        return this.apiCallUrl(url, method, body, cache);
+    }
+
+    public static apiCallT<T>(endpoint: string, method: string, resultClassRef: { new(): T }, body?: any): Promise<T> {
+        return this.apiCall(endpoint, method, body,
+            Environment.isBeta())
+            .then(NetworkUtil.deserializer(resultClassRef));
     }
 
     public static getSatappUrl(endpoint: string): string {
         const server = this.getServer();
-        return server + "/" + endpoint;
+        return server + (endpoint.startsWith("/") ? "" : "/") + endpoint;
     }
 
-    public static apiCallUrl(url: string, method: string, body?: any, prod?: boolean): Promise<any> {
-        return fetch(url, {
+    public static apiCallUrl(url: string, method: string, body?: any, cache: boolean = false): Promise<any> {
+        // TODO: Fetch with chacé, just for development, to avoid doing so much api calls and accelerating answers.
+        return NetworkUtil.fetch(url, {
             method: method,
             headers: {
                 'X-TripGo-Version': 'w3.2018.12.20',
@@ -46,8 +54,8 @@ class TripGoApi {
                 'Content-Type': 'application/json'
             },
             body: body ? JSON.stringify(body) : undefined
-        })
-            .then(NetworkUtil.jsonCallback);
+        }, cache)
+            // .then(NetworkUtil.jsonCallback); // TODO: Comment since NetworkUtil.fetch already calls jsonCallback.
     }
 
     public static updateRT(trip: Trip, query: RoutingQuery): Promise<Trip | undefined> {
@@ -60,16 +68,16 @@ class TripGoApi {
                 routingResults.setQuery(query);
                 routingResults.setSatappQuery(trip.satappQuery);
                 const tripGroups = routingResults.groups;
-                if (tripGroups.length === 0) {
+                if (tripGroups.length === 0 || tripGroups[0].trips.length === 0) {
                     throw new Error('Empty trip group.');
                 }
-                return tripGroups[0];
+                return tripGroups[0].trips[0];
             }).catch((reason: Error) => {
                 // Our api answers 200 with a null json when there is no update, so return undefined;
                 if (reason.message.includes("Unexpected end of JSON input")) {
                     return undefined;
                 }
-                console.log(reason);
+                Util.log(reason, Env.PRODUCTION);
                 throw reason;
             })
     }
