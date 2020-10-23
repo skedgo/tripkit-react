935d917186b65c491559632a7d7953c67aecb100
MauroGomez <mauro@buzzhives.com>
2020-10-23 19:11:37 -0300

    Fix to slide-up issue on touch devices.

--- src/trip-planner/TKUITripPlanner.css.ts
+++ src/trip-planner/TKUITripPlanner.css.ts
@@ -3,29 +3,37 @@ import {TKUITKUITripPlannerProps, TKUITKUITripPlannerStyle} from "./TKUITripPlan
 import TKUIResponsiveUtil from "../util/TKUIResponsiveUtil";
 import {cardSpacing, queryWidth, TKUITheme} from "../jss/TKUITheme";
 import genStyles from "../css/GenStyle.css";
-import DeviceUtil from "../util/DeviceUtil";
 
 export const tKUITripPlannerDefaultStyle: TKUIStyles<TKUITKUITripPlannerStyle, TKUITKUITripPlannerProps> =
     (theme: TKUITheme) => ({
         modalMain: {
             ...genStyles.flex,
             width: '100%',
-            // TODO!: Check this change for a while. Applied it also to portrait view to avoid unwanted scroll after
-            // adding position: relative (caused slide-up to behave bad on mobile: the part of the slide up that is
-            // hidden past the bottom of the screen (overflow) makes the whole screen to scroll until it becomes visible)
             ['@media (min-width: ' + (TKUIResponsiveUtil.getPortraitWidth() + 1) + 'px)']: {
                 height: '100%',
                 overflowY: 'hidden!important'
             },
-            // height: '100%',
-            // overflowY: 'hidden!important',
-            // -----------------------------------------------------------------------------------------------------
             '& input[type=text]': {
                 fontFamily: theme.fontFamily
             },
-            // This is so when the trip planner is embedded modals (including slide-ups) are positioned relative to
-            // this panel, and not to embedding app panel.
-            // position: 'relative'
+            /**
+             * For when trip planner is embedded:
+             * Set position relative so when the trip planner is embedded, modals (including slide-ups) are positioned relative to
+             * this panel, and not to embedding app panel.
+             * But position relative cause slide-up to behave bad on mobile (the part of the slide up that is
+             * hidden past the bottom of the screen (overflow) makes the whole screen to scroll until it becomes
+             * visible) so also need to add do the heigh: 100% and overflowY: hidden:
+             */
+            // position: 'relative',
+            // height: '100%',
+            // overflowY: 'hidden!important'
+            /**
+             * TODO!: fix issue: Relative positioning causes other issues on slide-up on mobile (breaks slide-up
+             * behaviour), so disable in general for now, and just enable when need to embed, e.g. for documentation.
+             * Other issue related to slide-up: it behaves smoother in a prev. version (viz.
+             * e6a0bdf3161c81495d28638d67b7c4c5f2316049 commit of 13/08/2020), but just for routing results view, which
+             * is very strange. See if I can get slide-up working like that again, and for all views.
+             */
         },
         main: {
             ...genStyles.flex,
