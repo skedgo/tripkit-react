a9d51576e5eac93c404b9b329c00f4b8baa18495
MauroGomez <mauro@buzzhives.com>
2020-11-24 12:21:28 -0300

    Changes on routing results model to be able to display JSON/s with some fields missing.

--- src/model/trip/Street.ts
+++ src/model/trip/Street.ts
@@ -4,8 +4,12 @@ import LeafletUtil from "../../util/LeafletUtil";
 
 @JsonObject
 class Street {
-    @JsonProperty("encodedWaypoints", String)
+    @JsonProperty("encodedWaypoints", String, true)
     private _encodedWaypoints: string = "";
+
+    @JsonProperty("waypoints", [Number], true)
+    private _decodedWaypoints: number[] | null = null;
+
     /**
      * Missing when unknown.
      */
@@ -44,8 +48,16 @@ class Street {
     }
 
     get waypoints(): LatLng[] | null {
-        if (this._waypoints === null && this._encodedWaypoints) {
-            this._waypoints = LeafletUtil.decodePolyline(this._encodedWaypoints);
+        if (this._waypoints === null) {
+            if (this._encodedWaypoints) {
+                this._waypoints = LeafletUtil.decodePolyline(this._encodedWaypoints);
+            } else if (this._decodedWaypoints) {
+                this._waypoints = [];
+                for (let i = 0; i < this._decodedWaypoints.length; i++) {
+                    this._waypoints.push(LatLng.createLatLng(this._decodedWaypoints[i], this._decodedWaypoints[i + 1]));
+                    i++;
+                }
+            }
         }
         return this._waypoints;
     }
