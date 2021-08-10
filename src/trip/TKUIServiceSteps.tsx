c87c273374d9bcda5a9d1038afe6b4b2b333e326
MauroGomez <mauro@buzzhives.com>
2021-08-10 17:01:56 -0300

    Feature #13578: TripKit React - Mode by Mode flow: - service detail (refactor to simplify render logic).

--- src/trip/TKUIServiceSteps.tsx
+++ src/trip/TKUIServiceSteps.tsx
@@ -41,48 +41,53 @@ class TKUIServiceSteps extends React.Component<IProps, {}> {
 
     public render(): React.ReactNode {
         const {steps, classes, timezone, t} = this.props;
-        // const stops = steps.reduce((stops: ServiceStopLocation[], shape: ServiceShape) =>
-        //     shape.stops ? stops.concat(shape.stops) : stops, []);
-        let stopIndex = -1;
+        const stops: ServiceStopLocation[] = [];
+        // Array mapping stop index to the 'travelled' flag of it's shape.
+        const travelledStops: boolean[] = [];
+        steps.forEach(shape => {
+            const shapeStops = shape.stops || [];
+            stops.push(...shapeStops);
+            shapeStops.forEach(() => travelledStops.push(shape.travelled));
+        });
+        const stepElements = stops.map((stop: ServiceStopLocation, stopI: number) => {
+            const leftLabel = stop.departure ?
+                DateTimeUtil.momentFromTimeTZ(stop.departure * 1000, timezone).format(DateTimeUtil.TIME_FORMAT_TRIP) :
+                stop.arrival ? DateTimeUtil.momentFromTimeTZ(stop.arrival * 1000, timezone).format(DateTimeUtil.TIME_FORMAT_TRIP) : "";
+            const ariaLabel = stop.name + ", " + t("At.X", {0: leftLabel}) + ". ";
+            const travelledStop = travelledStops[stopI];
+            const firstStop = stopI === 0;
+            const firstTravelledStop = travelledStop && (stopI === 0 || !travelledStops[stopI - 1]);
+            const lastStop = stopI === stops.length - 1;
+            const lastTravelledStop = travelledStop && (stopI === stops.length - 1 || !travelledStops[stopI + 1]);
+            return (
+                <div className={classes.step}
+                     key={stopI}
+                     tabIndex={0}
+                     aria-label={ariaLabel}
+                >
+                    <div className={classes.leftLabel}>
+                        {leftLabel}
+                    </div>
+                    <div className={classes.linePanel}>
+                        <div className={classNames(classes.line, travelledStop && !firstTravelledStop && classes.travelled,
+                            (!travelledStop || firstTravelledStop) && !firstStop && classes.untravelled,
+                            firstTravelledStop && "TKUIServiceSteps-firstTravelledStop")}/>
+                        <div className={classNames(classes.circle,
+                            travelledStop && classes.travelled, !travelledStop && classes.untravelled)}>
+                            <div/>
+                        </div>
+                        <div className={classNames(classes.line, travelledStop && !lastTravelledStop && classes.travelled,
+                            (!travelledStop || lastTravelledStop) && !lastStop && classes.untravelled)}/>
+                    </div>
+                    <div className={classes.rightLabel}>
+                        {stop.name}
+                    </div>
+                </div>
+            );
+        });
         return (
             <div id={this.id}>
-                {steps.reduce((stops: JSX.Element[], shape: ServiceShape, shapeI: number) =>
-                    stops.concat((shape.stops || []).map((stop: ServiceStopLocation, stopI: number) => {
-                        const leftLabel = stop.departure ?
-                            DateTimeUtil.momentFromTimeTZ(stop.departure * 1000, timezone).format(DateTimeUtil.TIME_FORMAT_TRIP) :
-                            stop.arrival ? DateTimeUtil.momentFromTimeTZ(stop.arrival * 1000, timezone).format(DateTimeUtil.TIME_FORMAT_TRIP) : "";
-                        const ariaLabel = stop.name + ", " + t("At.X", {0: leftLabel}) + ". ";
-                        stopIndex++;
-                        const firstStop = stopIndex === 0;
-                        const firstTravelledStop = stopI === 0 && shape.travelled && (shapeI === 0 || !steps[shapeI - 1].travelled);
-                        const lastStop = shapeI === steps.length - 1 && stopI === (shape.stops || []).length - 1;
-                        const lastTravelledStop = stopI === (shape.stops || []).length - 1 && shape.travelled && (shapeI === steps.length - 1 || !steps[shapeI + 1].travelled);
-                        return (
-                            <div className={classes.step}
-                                 key={stopIndex}
-                                 tabIndex={0}
-                                 aria-label={ariaLabel}
-                            >
-                                <div className={classes.leftLabel}>
-                                    {leftLabel}
-                                </div>
-                                <div className={classes.linePanel}>
-                                    <div className={classNames(classes.line, shape.travelled && !firstTravelledStop && classes.travelled,
-                                        (!shape.travelled || firstTravelledStop) && !firstStop && classes.untravelled,
-                                    firstTravelledStop && "TKUIServiceSteps-firstTravelledStop")}/>
-                                    <div className={classNames(classes.circle,
-                                        shape.travelled && classes.travelled, !shape.travelled && classes.untravelled)}>
-                                        <div/>
-                                    </div>
-                                    <div className={classNames(classes.line, shape.travelled && !lastTravelledStop && classes.travelled,
-                                        (!shape.travelled || lastTravelledStop) && !lastStop && classes.untravelled)}/>
-                                </div>
-                                <div className={classes.rightLabel}>
-                                    {stop.name}
-                                </div>
-                            </div>
-                        )})
-                    ), [])}
+                {stepElements}
             </div>
         );
     }
