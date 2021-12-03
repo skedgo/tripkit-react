b2a6b2815057d23df09e97d15dd951ce198799fa
MauroGomez <mauro@skedgo.com>
2021-12-03 19:23:41 -0300

    Bug #16171: Inconsistent service text colour. Feature #16174: Highlight tram and train different on the map. Feature #16176: Use color to differentiate different kinds of cycling paths.

--- src/model/location/ModeLocation.ts
+++ src/model/location/ModeLocation.ts
@@ -2,12 +2,13 @@ import {JsonObject, JsonProperty} from "json2typescript";
 import Location from "../Location";
 import ModeInfo from "../trip/ModeInfo";
 import TKDefaultGeocoderNames from "../../geocode/TKDefaultGeocoderNames";
+import Util from "../../util/Util";
 
 @JsonObject
 class ModeLocation extends Location {
 
     @JsonProperty("modeInfo", ModeInfo)
-    public modeInfo: ModeInfo = new ModeInfo();
+    public modeInfo: ModeInfo = Util.iAssign(new ModeInfo(), {localIcon: "parking"});
     @JsonProperty('source', String, true)
     public source: string | undefined = TKDefaultGeocoderNames.skedgo;
 
