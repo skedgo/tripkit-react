7a00feb23d2afc720a91b9acca799faabaea6c45
MauroGomez <mauro@skedgo.com>
2021-12-27 10:58:30 -0300

    Feature #16351: Display vehicle type as well as battery info when selecting a neuron vehicle on the map

--- src/map/TKUIMapLocationIcon.tsx
+++ src/map/TKUIMapLocationIcon.tsx
@@ -71,7 +71,8 @@ class TKUIMapLocationIcon extends React.PureComponent<IProps, {}> {
         if (location instanceof ModeLocation) {
             const modeInfo = location.modeInfo;
             const wantIconForDark = true;   // Always true, since pin background will always be dark (coloured).
-            const wantLocalIcon = !!modeInfo.identifier && modeInfo.identifier.startsWith("me_car-s");
+            const wantLocalIcon = !!modeInfo.identifier && 
+            (modeInfo.identifier.startsWith("me_car-s") || modeInfo.identifier.startsWith("cy_bic-s"));
             transIcon = TransportUtil.getTransportIcon(modeInfo, false, wantIconForDark);
             transIcon = TransportUtil.getTransIcon(modeInfo,
                 {
--- src/model/location/VehicleInfo.ts
+++ src/model/location/VehicleInfo.ts
@@ -1,8 +1,16 @@
-import {JsonObject} from "json2typescript";
+import {JsonObject, JsonProperty} from "json2typescript";
+import CompanyInfo from "./CompanyInfo";
 
 @JsonObject
 class VehicleInfo {
+    @JsonProperty("batteryLevel", Number, true)
+    public batteryLevel?: number = undefined;
 
+    @JsonProperty("lastUpdate", Number, true)
+    public lastUpdate?: number = undefined;
+
+    @JsonProperty("operator", CompanyInfo)
+    public operator: CompanyInfo = new CompanyInfo();
 }
 
 export default VehicleInfo;
