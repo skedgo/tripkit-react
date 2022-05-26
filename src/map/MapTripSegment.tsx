23a2de1d737e09d1fb7558319d06ee30d79e0d4e
MauroGomez <mauro@skedgo.com>
2022-05-26 10:31:36 -0300

    RM17306: Cycling turn by turn directions and key.

--- src/map/MapTripSegment.tsx
+++ src/map/MapTripSegment.tsx
@@ -1,19 +1,19 @@
 import * as React from "react";
 import Segment from "../model/trip/Segment";
-import {Marker, Popup} from "react-leaflet";
-import {Visibility} from "../model/trip/SegmentTemplate";
+import { Marker, Popup } from "react-leaflet";
+import { Visibility } from "../model/trip/SegmentTemplate";
 import L from "leaflet";
 import LatLng from "../model/LatLng";
 import TKUIMapStreets from "./TKUIMapStreets";
 import TKUIMapShapes from "./TKUIMapShapes";
-import {TKUIMapViewClass} from "./TKUIMapView";
+import { TKUIMapViewClass } from "./TKUIMapView";
 import TKUIRealtimeVehicle from "./TKUIRealtimeVehicle";
-import {TKUIConfig} from "../config/TKUIConfig";
-import {TKUIConfigContext, default as TKUIConfigProvider, TKUIThemeConsumer} from "../config/TKUIConfigProvider";
-import {TKUITransportPin} from "./TKUITransportPin";
-import {TKUITheme} from "../jss/TKUITheme";
+import { TKUIConfig } from "../config/TKUIConfig";
+import { TKUIConfigContext, default as TKUIConfigProvider, TKUIThemeConsumer } from "../config/TKUIConfigProvider";
+import { TKUITransportPin } from "./TKUITransportPin";
+import { TKUITheme } from "../jss/TKUITheme";
 import SegmentPopup from "./SegmentPopup";
-import {TranslationFunction} from "../i18n/TKI18nProvider";
+import { TranslationFunction } from "../i18n/TKI18nProvider";
 import { renderToStaticMarkup } from "../jss/StyleHelper";
 
 interface IProps {
@@ -47,65 +47,65 @@ class MapTripSegment extends React.Component<IProps, {}> {
                             return [
                                 segment.hasVisibility(Visibility.ON_MAP) &&
                                 <Marker icon={icon} position={segment.from} key={"pin"}
-                                        draggable={this.props.ondragend !== undefined}
-                                        riseOnHover={segment.isFirst(Visibility.IN_SUMMARY)}
-                                        ondragend={(event: L.DragEndEvent) => {
-                                            if (this.props.ondragend) {
-                                                const latLng = event.target.getLatLng();
-                                                this.props.ondragend(LatLng.createLatLng(latLng.lat, latLng.lng));
-                                            }
-                                        }}
-                                        keyboard={false}
+                                    draggable={this.props.ondragend !== undefined}
+                                    riseOnHover={segment.isFirst(Visibility.IN_SUMMARY)}
+                                    ondragend={(event: L.DragEndEvent) => {
+                                        if (this.props.ondragend) {
+                                            const latLng = event.target.getLatLng();
+                                            this.props.ondragend(LatLng.createLatLng(latLng.lat, latLng.lng));
+                                        }
+                                    }}
+                                    keyboard={false}
                                 >
                                     <Popup offset={[0, -46]}
-                                           closeButton={false}
+                                        closeButton={false}
                                         // TODO: disabled auto pan to fit popup on open since it messes with viewport
                                         // (generates infinite (or a lot) setState calls) since it seems the viewport
                                         // doesn't stabilizes. Fix it.
-                                           autoPan={false}
+                                        autoPan={false}
                                     >
-                                        {<SegmentPopup segment={segment} t={this.props.t}/>}
+                                        {<SegmentPopup segment={segment} t={this.props.t} />}
                                     </Popup>
                                 </Marker>,
                                 segment.shapes ?
                                     <TKUIMapShapes key={"map-polyline" + segment.trip.getKey() + segment.id}
-                                                   id={"map-polyline" + segment.trip.getKey() + segment.id}
-                                                   shapes={segment.shapes}
-                                                   color={segment.getColor()}
+                                        id={"map-polyline" + segment.trip.getKey() + segment.id}
+                                        shapes={segment.shapes}
+                                        color={segment.getColor()}
                                     /> :
                                     segment.streets ?
                                         <TKUIMapStreets key={"map-polyline" + segment.trip.getKey() + segment.id}
-                                                        id={"map-polyline" + segment.trip.getKey() + segment.id}
-                                                        color={segment.isWalking() ? "#20ce6e" :
-                                                            (segment.isBicycle() && segment.streets.length > 0 && segment.streets[0].safe !== null
-                                                                ? undefined : segment.getColor())}
-                                                        modeInfo={segment.modeInfo}
-                                                        streets={segment.streets}
+                                            id={"map-polyline" + segment.trip.getKey() + segment.id}
+                                            color={segment.isWalking() ? "#20ce6e" :
+                                                ((segment.isBicycle() || segment.isWheelchair()) && segment.streets.length > 0
+                                                    ? undefined : segment.getColor())}
+                                            modeInfo={segment.modeInfo}
+                                            streets={segment.streets}
 
                                         /> : undefined,
                                 segment.realtimeVehicle &&
                                 // (DateTimeUtil.getNow().valueOf() / 1000 - segment.realtimeVehicle.lastUpdate) < 120 &&
                                 <Marker position={segment.realtimeVehicle.location}
-                                        key={"vehicle"}
-                                        icon={L.divIcon({
-                                            html: renderToStaticMarkup(
-                                                <TKUIConfigProvider config={config}>
-                                                    <TKUIRealtimeVehicle
-                                                        value={segment.realtimeVehicle}
-                                                        label={segment.serviceNumber || undefined}
-                                                        color={segment.serviceColor || undefined}
-                                                    />
-                                                </TKUIConfigProvider>
-                                            ),
-                                            iconSize: [40, 40],
-                                            iconAnchor: [20, 20],
-                                            className: this.props.vehicleClassName
-                                        })}
-                                        riseOnHover={true}
-                                        keyboard={false}
+                                    key={"vehicle"}
+                                    icon={L.divIcon({
+                                        html: renderToStaticMarkup(
+                                            <TKUIConfigProvider config={config}>
+                                                <TKUIRealtimeVehicle
+                                                    value={segment.realtimeVehicle}
+                                                    label={segment.serviceNumber || undefined}
+                                                    color={segment.serviceColor || undefined}
+                                                />
+                                            </TKUIConfigProvider>
+                                        ),
+                                        iconSize: [40, 40],
+                                        iconAnchor: [20, 20],
+                                        className: this.props.vehicleClassName
+                                    })}
+                                    riseOnHover={true}
+                                    keyboard={false}
                                 >
                                     {segment.modeInfo && segment.serviceNumber &&
-                                    TKUIMapViewClass.getPopup(segment.realtimeVehicle, segment.modeInfo.alt + " " + segment.serviceNumber)}
+                                        TKUIMapViewClass.getPopup(segment.realtimeVehicle, segment.modeInfo.alt + " " + segment.serviceNumber)}
                                 </Marker>
                             ];
                         }}
