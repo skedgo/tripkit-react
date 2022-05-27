5aacc02d80d61f736710544756d8e7976ca52abc
MauroGomez <mauro@skedgo.com>
2022-05-27 14:06:47 -0300

    Work on TripKit React Doc.

--- src/example/tripgo/TGApp.tsx
+++ src/example/tripgo/TGApp.tsx
@@ -1,25 +1,33 @@
-import React, {useState, useEffect} from 'react';
-import {CSSProps, TKUIWithStyle} from "../../jss/StyleHelper";
-import {ReactComponent as IconReport} from './images/icon-usersnap.svg';
+import React from 'react';
+import { CSSProps, TKUIWithStyle } from "../../jss/StyleHelper";
+import { ReactComponent as IconReport } from './images/icon-usersnap.svg';
 import Usersnap from "./usersnap/Usersnap";
-import RequestSupportAction from "./feedback/RequestSupportAction";
-import TKErrorHelper from "../../error/TKErrorHelper";
-import {ERROR_ROUTING_NOT_SUPPORTED, ERROR_DESTINATION_OUTSIDE_COVERAGE} from "../../error/TKErrorHelper";
 import TKStateConsumer from "../../config/TKStateConsumer";
-import {TKState} from "../../config/TKState";
-import {TKUITimetableViewProps} from "../../service/TKUITimetableView";
-import {TKUIMapViewProps} from "../../map/TKUIMapView";
+import { TKState } from "../../config/TKState";
+import { TKUITimetableViewProps } from "../../service/TKUITimetableView";
+import { TKUIMapViewProps } from "../../map/TKUIMapView";
 import TKStateUrl from "../../state/TKStateUrl";
 import TKGeocodingOptions from "../../geocode/TKGeocodingOptions";
-import TKUISettingLink from "../../options/TKUISettingLink";
-import {IOptionsContext, default as OptionsProvider, OptionsContext} from "../../options/OptionsProvider";
-import {getApiKey, default as TGUIDevSettingsView, getServer, isTfGMReferrer} from "./options/TGUIDevSettingsView";
-import TKUISettingSection from "../../options/TKUISettingSection";
-import RegionsData from "../../data/RegionsData";
-import OptionsData from "../../data/OptionsData";
-import {loadTripState} from "./options/TGUILoadTripsView";
-import {ReactComponent as TripgoLogo} from './images/logo-tripgo.svg';
-import {ReactComponent as TripgoLogoDark} from './images/logo-tripgo-dark.svg';
+import { ReactComponent as TripgoLogo } from './images/logo-tripgo.svg';
+import { ReactComponent as TripgoLogoDark } from './images/logo-tripgo-dark.svg';
+import Environment from "../../env/Environment";
+import { default as TKPeliasGeocoder } from "../../geocode/PeliasGeocoder";
+import { TKError } from "../../error/TKError";
+import { TKUIConfig } from "../../config/TKUIConfig";
+import TKUIButton from "../../buttons/TKUIButton";
+import LatLng from "../../model/LatLng";
+import TKRoot from "../../config/TKRoot";
+import TKUITripPlanner from "../../trip-planner/TKUITripPlanner";
+// import {
+//     TKUITripPlanner,
+//     TKRoot,
+//     TKUIConfig,
+//     LatLng,
+//     Environment,
+//     TKPeliasGeocoder, TKError, TKUIRoutingResultsViewProps,
+//     TKUIButton}
+//     from '../../index';
+// from 'tripkit-react';
 
 export interface IClientProps extends TKUIWithStyle<IStyle, IProps> {
 }
@@ -28,7 +36,7 @@ export interface IStyle {
     main: CSSProps<IProps>;
 }
 
-interface IProps {}
+interface IProps { }
 
 const TGApp: React.SFC<IProps> = (props: IProps) => {
 
@@ -44,21 +52,10 @@ const TGApp: React.SFC<IProps> = (props: IProps) => {
     const geocodeEarth = new TKPeliasGeocoder({
         server: "https://api.geocode.earth/v1",
         apiKey: "ge-63f76914953caba8",
-        restrictToBounds: isTfGMReferrer(),
         resultsLimit: 5
     });
 
     const hostname = window.location.hostname;
-    const devSettings = hostname.includes("tripkit.") || hostname.includes("beta.") || hostname.includes("localhost");
-
-    const [showDevSettings, setShowDevSettings] = useState<boolean>(false);
-    const [showLoadTrips, setShowLoadTrips] = useState<boolean>(false);
-    const devSettingsView = showDevSettings &&
-        <TGUIDevSettingsView
-            showLoadTrips={showLoadTrips}
-            setShowLoadTrips={setShowLoadTrips}
-            onRequestClose={() => setShowDevSettings(false)}
-        />;
     const config: TKUIConfig = {
         apiKey: '790892d5eae024712cfd8616496d7317',
         theme: {
@@ -71,8 +68,7 @@ const TGApp: React.SFC<IProps> = (props: IProps) => {
             geocoders: {
                 ...defaultOptions.geocoders,
                 'geocodeEarth': geocodeEarth
-            },
-            ...isTfGMReferrer() && {restrictToCoverageBounds: true}
+            }
         }),
         TKUIReportBtn: {
             // Good example of element replacement.
@@ -87,7 +83,7 @@ const TGApp: React.SFC<IProps> = (props: IProps) => {
             // Other alternative, preserve all features on TKUIReportBtn
             // (viz. copy to clipboard on right click).
             props: {
-                renderIcon: () => <IconReport/>,
+                renderIcon: () => <IconReport />,
                 onClick: (state: TKState) => {
                     Usersnap.setFeedbackData(state);
                     Usersnap.openReportWindow();
@@ -96,7 +92,7 @@ const TGApp: React.SFC<IProps> = (props: IProps) => {
         },
         TKUIMapView: {
             props: (props: TKUIMapViewProps) => {
-                const {trip, tripSegment} = props;
+                const { trip, tripSegment } = props;
                 const mapTiles = tripSegment ? tripSegment.mapTiles : trip?.mainSegment?.mapTiles;
                 return (mapTiles ?
                     {
@@ -124,42 +120,6 @@ const TGApp: React.SFC<IProps> = (props: IProps) => {
                 );
             }
         },
-        TKUIRoutingResultsView: {
-            props: (props: TKUIRoutingResultsViewProps) => ({
-                errorActions: (error: TKError, defaultActions: JSX.Element[]) => {
-                    let errorActions = defaultActions;
-                    const query = props.query;
-                    if (TKErrorHelper.hasErrorCode(error, ERROR_ROUTING_NOT_SUPPORTED) ||
-                        TKErrorHelper.hasErrorCode(error, ERROR_DESTINATION_OUTSIDE_COVERAGE)) {
-                        errorActions = [
-                            <RequestSupportAction
-                                actionTitle={props.t("Request.support")}
-                                formTitle={"Request route"}
-                                formMessage={"Please support routing between " + query.from!.address + " and " + query.to!.address + "\n\n" +
-                                "From coordinate: " + query.from!.lat + ", " + query.from!.lng + "\n" +
-                                "To coordinate: " + query.to!.lat + ", " + query.to!.lng}
-                                key={"Request.support"}
-                            />
-                        ].concat(errorActions);
-                    } else {
-                        errorActions = [
-                            <TKStateConsumer>
-                                {(state: TKState) =>
-                                    <TKUIButton
-                                        text={props.t("Report.Problem")}
-                                        onClick={() => {
-                                            Usersnap.setFeedbackData(state);
-                                            Usersnap.openReportWindow();
-                                        }}
-                                    />
-                                }
-                            </TKStateConsumer>
-                        ].concat(errorActions);
-                    }
-                    return errorActions;
-                }
-            })
-        },
         TKUITimetableView: {
             props: (props: TKUITimetableViewProps) => ({
                 errorActions: (error: TKError) => ([
@@ -177,60 +137,18 @@ const TGApp: React.SFC<IProps> = (props: IProps) => {
                 ].concat(props.errorActions ? props.errorActions(error) : []))
             })
         },
-        TKUIProfileView: {
-            props: devSettings ? {
-                customSettings: () =>
-                    <TKUISettingSection>
-                        <TKUISettingLink text={"Beta Testing"} onClick={() => setShowDevSettings(true)}/>
-                    </TKUISettingSection>
-
-            } : undefined
-        },
         TKUITripPlanner: {
             props: (props) => ({
                 renderTopRight: props.landscape ?
-                    (props.theme.isDark ? () => <TripgoLogoDark/> : () => <TripgoLogo/>) : undefined
+                    (props.theme.isDark ? () => <TripgoLogoDark /> : () => <TripgoLogo />) : undefined
             })
         },
         TKUISidebar: {
             props: {
-                renderLogo: () => <TripgoLogo/>,
+                renderLogo: () => <TripgoLogo />,
                 appStoreUrl: "https://apps.apple.com/au/app/tripgo/id533630842",
                 playStoreUrl: "https://play.google.com/store/apps/details?id=com.buzzhives.android.tripplanner"
             }
-        },
-        onInitState: (tKState: TKState) => {
-            RegionsData.instance.requireRegions().catch(error => {
-                const userProfile = OptionsData.instance.get();
-                if (error.message.includes("Invalid API key")) {
-                    const invalidDevKey = userProfile.customData && userProfile.customData.apiKey;
-                    if (invalidDevKey) {
-                        if (window.confirm("Invalid API key: " + invalidDevKey + ". Will reset to production key and reload.")) {
-                            delete userProfile.customData.apiKey;
-                            OptionsData.instance.save(userProfile);
-                            window.location.reload();
-                        }
-                    }
-                } else {
-                    const invalidServer = userProfile.customData && userProfile.customData.server;
-                    if (invalidServer) {
-                        if (window.confirm("There was a problem loading regions from server: " + invalidServer + ". Will reset to production and reload.")) {
-                            delete userProfile.customData.server;
-                            OptionsData.instance.save(userProfile);
-                            window.location.reload();
-                        }
-                    }
-                }
-                throw error;
-            });
-            document.addEventListener("keydown", (zEvent: any) => {
-                if (zEvent.shiftKey && zEvent.metaKey && zEvent.key === "v") {
-                    navigator.clipboard && navigator.clipboard.readText && navigator.clipboard.readText().then(t => {
-                        loadTripState(t, tKState);
-                    }).catch((e) => console.log(e));
-                    zEvent.preventDefault();
-                }
-            });
         }
     };
 
@@ -243,37 +161,11 @@ const TGApp: React.SFC<IProps> = (props: IProps) => {
         userLocationPromise = Promise.reject();
     }
 
-    useEffect(() => {
-        const keyEventListener = (zEvent: any) => {
-            if (zEvent.shiftKey && zEvent.metaKey && (zEvent.key === "d" || zEvent.key === "s")) {
-                setShowDevSettings(true);
-                zEvent.preventDefault();
-            } else if (zEvent.shiftKey && zEvent.metaKey && (zEvent.key === "p" || zEvent.key === "t" || zEvent.key === "l")) {
-                setShowLoadTrips(true);
-                setShowDevSettings(true);
-                zEvent.preventDefault();
-            }
-        };
-        document.addEventListener("keydown", keyEventListener);
-        return () => {
-            document.removeEventListener("keydown", keyEventListener);
-        };
-    });
-
-    // Obs: OptionsProvider used here and inside TKRoot keep their values at sync due to OptionsProvider subscribing to
-    // OptionsData changes (See OptionsProvider:36). Try to avoid this so can remove that subscription.
     return (
-        <OptionsProvider>
-            <OptionsContext.Consumer>
-                {(optionsContext: IOptionsContext) =>
-                    <TKRoot config={{...config, apiKey: getApiKey(optionsContext.userProfile), server: getServer(optionsContext.userProfile)}}>
-                        <TKUITripPlanner userLocationPromise={userLocationPromise}/>
-                        <TKStateUrl/>
-                        {devSettingsView}
-                    </TKRoot>
-                }
-            </OptionsContext.Consumer>
-        </OptionsProvider>
+        <TKRoot config={config}>
+            <TKUITripPlanner userLocationPromise={userLocationPromise} />
+            <TKStateUrl />
+        </TKRoot>
     );
 };
 
