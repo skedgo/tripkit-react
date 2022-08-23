0490f60c54e280c69c25988eaa2301974ac518ba
MauroGomez <mauro@skedgo.com>
2022-08-23 19:12:37 -0300

    RM17741: Allow picking different scooter

---
+++ src/mxm/TKUIMxMCollectNearbyCard.css.ts
@@ -0,0 +1,8 @@
+import genStyles from "../css/GenStyle.css";
+import { TKUITheme } from "../jss/TKUITheme";
+
+export const tKUIMxMCollectNearbyCardDefaultStyle = (theme: TKUITheme) => ({
+    main: {
+        ...genStyles.scrollableY
+    }
+});
---
+++ src/mxm/TKUIMxMCollectNearbyCard.tsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react';
+import TripGoApi from '../api/TripGoApi';
+import TKUICard from '../card/TKUICard';
+import { connect, mapperFromFunction } from "../config/TKConfigHelper";
+import { TKComponentDefaultConfig, TKUIConfig } from '../config/TKUIConfig';
+import { overrideClass, TKUIWithClasses, TKUIWithStyle } from '../jss/StyleHelper';
+import LocationsResult from '../model/location/LocationsResult';
+import Segment from '../model/trip/Segment';
+import NetworkUtil from '../util/NetworkUtil';
+import Util from '../util/Util';
+import { tKUIMxMCollectNearbyCardDefaultStyle } from './TKUIMxMCollectNearbyCard.css';
+import ModeLocation from '../model/location/ModeLocation';
+import TKUIModeLocationRow from './TKUIModeLocationRow';
+
+type IStyle = ReturnType<typeof tKUIMxMCollectNearbyCardDefaultStyle>
+
+interface IClientProps extends TKUIWithStyle<IStyle, IProps> {
+    segment: Segment;
+    onRequestClose: () => void;
+}
+
+interface IProps extends IClientProps, TKUIWithClasses<IStyle, IProps> { }
+
+export type TKUIMxMCollectNearbyCardProps = IProps;
+export type TKUIMxMCollectNearbyCardStyle = IStyle;
+
+const config: TKComponentDefaultConfig<IProps, IStyle> = {
+    render: props => <TKUIMxMCollectNearbyCard {...props} />,
+    styles: tKUIMxMCollectNearbyCardDefaultStyle,
+    classNamePrefix: "TKUIMxMBookingCard"
+};
+
+const TKUIMxMCollectNearbyCard: React.FunctionComponent<IProps> = ({ segment, onRequestClose, classes, injectedStyles, t }) => {
+    const [alternatives, setAlternatives] = useState<ModeLocation[] | undefined>(undefined);
+    useEffect(() => {
+        const mode = segment.modeIdentifier ?? segment.modeInfo?.identifier;
+        TripGoApi.apiCall(`locations.json?lat=${segment.from.lat}&lng=${segment.from.lng}&radius=500&modes=${mode}&strictModeMatch=false`, NetworkUtil.MethodType.GET)
+            .then(groupsJSON => Util.deserialize(groupsJSON.groups[0], LocationsResult))
+            .then(locationsResult => setAlternatives(locationsResult.getLocations()));
+    }, [segment])
+    return (
+        <TKUICard
+            title={segment.getAction()}
+            styles={{
+                main: overrideClass({ height: '100%', position: 'relative' })
+            }}
+        >
+            {/* <div className={classes.main}> */}
+                {alternatives?.map((alt, i) =>
+                    <TKUIModeLocationRow location={alt} key={i} />)}
+            {/* </div> */}
+        </TKUICard>
+    )
+}
+
+export default connect((config: TKUIConfig) => config.TKUIMxMCollectNearbyCard, config,
+    mapperFromFunction((clientProps: IClientProps) => clientProps));
