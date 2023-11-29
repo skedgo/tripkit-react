f0661ecee46ac0bda43d474255aabd8fdcaa962c
MauroGomez <mauro@skedgo.com>
2023-11-29 16:35:44 -0300

    Render card title div just if a title was provided

--- src/card/TKUICardHeader.tsx
+++ src/card/TKUICardHeader.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
-import {TKUIWithClasses, TKUIWithStyle} from "../jss/StyleHelper";
-import {ReactComponent as IconRemove} from '../images/ic-cross2.svg';
-import {tKUICardHeaderJss} from "./TKUICardHeader.css";
-import {TKComponentDefaultConfig, TKUIConfig} from "../config/TKUIConfig";
-import {connect, mapperFromFunction} from "../config/TKConfigHelper";
+import { TKUIWithClasses, TKUIWithStyle } from "../jss/StyleHelper";
+import { ReactComponent as IconRemove } from '../images/ic-cross2.svg';
+import { tKUICardHeaderJss } from "./TKUICardHeader.css";
+import { TKComponentDefaultConfig, TKUIConfig } from "../config/TKUIConfig";
+import { connect, mapperFromFunction } from "../config/TKConfigHelper";
 
 type IStyle = ReturnType<typeof tKUICardHeaderJss>
 
@@ -15,40 +15,41 @@ export interface TKUICardHeaderClientProps {
     noPaddingTop?: boolean;
 }
 
-interface IClientProps extends TKUICardHeaderClientProps, TKUIWithStyle<IStyle, IProps> {}
+interface IClientProps extends TKUICardHeaderClientProps, TKUIWithStyle<IStyle, IProps> { }
 
-interface IProps extends IClientProps, TKUIWithClasses<IStyle, IProps> {}
+interface IProps extends IClientProps, TKUIWithClasses<IStyle, IProps> { }
 
 export type TKUICardHeaderProps = IProps;
 export type TKUICardHeaderStyle = IStyle;
 
 const config: TKComponentDefaultConfig<IProps, IStyle> = {
-    render: props => <TKUICardHeader {...props}/>,
+    render: props => <TKUICardHeader {...props} />,
     styles: tKUICardHeaderJss,
     classNamePrefix: "TKUICardHeader"
 };
 
 
 const TKUICardHeader: React.FunctionComponent<IProps> = (props: IProps) => {
-    const {title, subtitle, onRequestClose, classes, closeAriaLabel} = props;
+    const { title, subtitle, onRequestClose, classes, closeAriaLabel } = props;
     return (
         <div className={classes.main}>
             <div className={classes.headerTop}>
-                <div className={classes.title} id={"tkui-card-title"}>
-                    {title}
-                </div>
+                {title &&
+                    <div className={classes.title} id={"tkui-card-title"}>
+                        {title}
+                    </div>}
                 {onRequestClose &&
-                <button onClick={onRequestClose} className={classes.btnClear}
+                    <button onClick={onRequestClose} className={classes.btnClear}
                         aria-label={closeAriaLabel || "Close"}>
-                    <IconRemove aria-hidden={true}
-                                className={classes.iconClear}
-                                focusable="false"/>
-                </button>}
+                        <IconRemove aria-hidden={true}
+                            className={classes.iconClear}
+                            focusable="false" />
+                    </button>}
             </div>
             {subtitle &&
-            <div className={classes.subtitle}>
-                {subtitle}
-            </div>}
+                <div className={classes.subtitle}>
+                    {subtitle}
+                </div>}
         </div>
     );
 };
