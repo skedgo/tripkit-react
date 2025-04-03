abcdb56f88767d4ce18de26043464d699e66ad44
MauroGomez <mauro@skedgo.com>
2025-04-03 12:54:13 -0300

    Upgrade to react 18 (2)

--- src/util_components/ReactResizeDetector.tsx
+++ src/util_components/ReactResizeDetector.tsx
@@ -1,5 +1,8 @@
 import React from "react";
-import useResizeDetector from "react-resize-detector/build/useResizeDetector";
+import { useResizeDetector } from "react-resize-detector";
+// If the previous import fails, a workaraound to make it work locally is with 
+// the following line, removing the export property in react-resize-detector's package.json in node-modules.
+// import useResizeDetector from "react-resize-detector/build/useResizeDetector";
 
 const ReactResizeDetector = (props: { handleWidth?: boolean, handleHeight?: boolean, targetRef?: any, onResize?: () => void }) => {
     useResizeDetector(props);
