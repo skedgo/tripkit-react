5ee5de3e3fbf369e3a0c6edf8849bb587381aa5e
MauroGomez <mgomezlucero@gmail.com>
2026-06-10 13:16:01 -0300

    Upgrade react types

--- src/build-info.ts
+++ src/build-info.ts
@@ -1,2 +1,2 @@
-export const TRIPKIT_REACT_BUILD_SHA = "<BUILD_SHA>" as const;
-export const TRIPKIT_REACT_BUILD_DATE = "<BUILD_DATE>" as const;
+export const TRIPKIT_REACT_BUILD_SHA = "unknown" as const;
+export const TRIPKIT_REACT_BUILD_DATE = "2026-06-10T13:18:11.094Z" as const;
